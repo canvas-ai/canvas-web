54e7cf18fc5a726ea06239f4aa08e1f41d8fe35d
Tarbor <tarborlevan@gmail.com>
2025-01-21 09:42:21 +0400

    add the github link

--- src/components/auth/auth-layout.tsx
+++ src/components/auth/auth-layout.tsx
@@ -8,8 +8,8 @@ interface AuthLayoutProps {
 
 export function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center p-4">
-      <Card className="flex w-[95vw] h-[95vh] overflow-hidden">
+    <div className="flex min-h-screen w-full items-center justify-center p-4 flex-col">
+      <Card className="flex w-[95vw] h-[90vh] overflow-hidden">
         <div className="hidden md:block w-1/2 relative bg-black">
           <Link to="/">
             <a  className="absolute top-8 left-8 font-bold text-white flex items-center">
@@ -27,6 +27,10 @@ export function AuthLayout({ children }: AuthLayoutProps) {
           </div>
         </div>
       </Card>
+      <a href="https://github.com/canvas-ai" target="_blank" className="mt-4 text-gray-600 hover:text-gray-900 flex items-center space-x-2" rel="noopener noreferrer">
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-6 h-6"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+        <span>GitHub</span>
+      </a>
     </div>
   )
 }
